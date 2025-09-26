36640334e15dff56e3b865449259cb09f880a993
Mizyck <Mizycku@outlook.com>
2025-09-27 01:27:52 +0800

    feat：添加CosyVoice作为tts，更新readme、模型下载脚本

--- services/asr_service.py
+++ services/asr_service.py
@@ -1,8 +1,11 @@
 from funasr import AutoModel
 import time
+import os
+
+model_dir = os.path.join(os.getcwd(), "models", "asr")
 
 # 加载模型
-asr_model = AutoModel(model = "paraformer-zh", vad_model = "fsmn-vad", punc_model = "ct-punc")
+asr_model = AutoModel(model = "paraformer-zh", vad_model = "fsmn-vad", punc_model = "ct-punc", cache_dir = model_dir)
 
 def transcribe_audio(file_path:str):
     start = time.time()
