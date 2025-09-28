e9631cfcf0e99bcb6ef8419031297cdfe9b988bd
Mizyck <Mizycku@outlook.com>
2025-09-28 17:33:49 +0800

    fix: 为config.py添加注释

--- core/config.py
+++ core/config.py
@@ -5,6 +5,6 @@ load_dotenv()  # 加载 .env 文件
 
 class Settings:
     QINIU_API_KEY: str = os.getenv("QINIU_API_KEY")
-    QINIU_BASE_URL: str = "https://openai.qiniu.com/v1"
+    QINIU_BASE_URL: str = "https://openai.qiniu.com/v1"     # 默认使用七牛云接口，可自行修改
 
 settings = Settings()
